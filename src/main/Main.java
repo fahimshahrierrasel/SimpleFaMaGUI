99dcfff9f1309c972cd48944539bd7e6f3dd534f
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-03-17 21:05:55 +0600

    FaMaSDK testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,29 @@
 package main;
 
+import es.us.isa.FAMA.Reasoner.QuestionTrader;
+import es.us.isa.FAMA.Reasoner.questions.NumberOfProductsQuestion;
+import es.us.isa.FAMA.Reasoner.questions.ValidQuestion;
+import es.us.isa.FAMA.models.variabilityModel.VariabilityModel;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Simple FaMa GUI");
+		// The main class is instantiated
+		QuestionTrader qt = new QuestionTrader();
+
+		// A feature model is loaded
+		VariabilityModel fm = qt.openFile("fm-samples/test.fama");
+		qt.setVariabilityModel(fm);
+
+		////////// VALID QUESTION + NUMBER PRODUCTS QUESTION ///////////
+		ValidQuestion vq = (ValidQuestion) qt.createQuestion("Valid");
+		qt.ask(vq);
+		if (vq.isValid()) {
+			NumberOfProductsQuestion npq = (NumberOfProductsQuestion) qt.createQuestion("#Products");
+			qt.ask(npq);
+			System.out.println("The number of products is: " + npq.getNumberOfProducts());
+		} else {
+			System.out.println("Your feature model is not valid");
+		}
 	}
 }
